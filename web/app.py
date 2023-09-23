bd15a36a33211c9372bcb947c3bcca40b19e01c9
vchhajed <vchhajed@buffalo.edu>
2023-09-23 15:01:28 -0700

    added env

---
+++ web/app.py
@@ -0,0 +1,55 @@
+import streamlit as st
+import fitz  # PyMuPDF
+import tempfile
+import os
+import base64
+
+def pdf_to_markdown(pdf_file):
+    doc = fitz.open(pdf_file)
+    print(doc)
+    markdown_text = ""
+
+    for page_num in range(len(doc)):
+        page = doc.load_page(page_num)
+        markdown_text += page.get_text("text")
+
+    return markdown_text
+
+st.title("PDF to Markdown Converter")
+
+# Upload a PDF file
+pdf_file = st.file_uploader("Upload a PDF file", type=["pdf"])
+
+if pdf_file is not None:
+    # Display the uploaded PDF
+    st.subheader("Uploaded PDF:")
+    st.write(pdf_file)
+
+    # Create a temporary file for the uploaded PDF
+    pdf_temp_file = tempfile.NamedTemporaryFile(delete=False, suffix=".pdf")
+    pdf_temp_file.write(pdf_file.read())
+    pdf_temp_file.close()
+
+    # Process the PDF file
+    markdown_text = pdf_to_markdown(pdf_temp_file.name)
+
+    # Display the Markdown text
+    st.subheader("Markdown Output:")
+    st.text(markdown_text)
+
+    # Provide a download link for the uploaded PDF
+    st.markdown("### Download Uploaded PDF")
+    st.markdown(f"Download the uploaded PDF [here](data:application/pdf;base64,{base64.b64encode(open(pdf_temp_file.name, 'rb').read()).decode()})")
+
+    # Create a temporary file for the generated Markdown
+    markdown_temp_file = tempfile.NamedTemporaryFile(delete=False, suffix=".md")
+    markdown_temp_file.write(markdown_text.encode())
+    markdown_temp_file.close()
+
+    # Provide a download link for the generated Markdown
+    st.markdown("### Download Markdown")
+    st.markdown(f"Download the generated Markdown [here](data:file/md;base64,{base64.b64encode(open(markdown_temp_file.name, 'rb').read()).decode()})")
+
+    # Remove the temporary files
+    os.unlink(pdf_temp_file.name)
+    os.unlink(markdown_temp_file.name)
