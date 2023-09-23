2297bbfc4f13d2c6335ab74f853b7788de2db817
Jordan Parker <jordanhparker6@gmail.com>
2023-09-23 15:43:50 -0700

    tada

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
-from services.langchain.chains import load_chain
+from services.langchain.chains import load_critisim_chain
 from dotenv import load_dotenv
 
 load_dotenv()
 
-chain = load_chain()
+chain = load_critisim_chain()
 with open("markdown/sample.md") as f:
     SAMPLE_DOCUMENT = f.read()
 
