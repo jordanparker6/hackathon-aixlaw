25da6bb0ea8a763227179a0c5c9ea4291bb285a3
Jordan Parker <jordanhparker6@gmail.com>
2023-09-23 18:30:25 -0700

    adad

--- app.py
+++ app.py
@@ -45,26 +45,26 @@ if len(msgs.messages) == 0 or st.sidebar.button("Reset chat history"):
     msgs.clear()
     st.session_state.steps = {}
 
-avatars = {"human": "user", "ai": "assistant"}
-for idx, msg in enumerate(msgs.messages):
-    with st.chat_message(avatars[msg.type]):
-        # Render intermediate steps if any were saved
-        for step in st.session_state.steps.get(str(idx), []):
-            log.info("step", step=step)
-            if step[0].tool == "_Exception":
-                continue
-            query = step[0].tool_input["query"]
-            with st.status(label=f"**Critisim**: {query}", state="running"):
-                 st.write("running")
-            with st.status(label=f"**Critisim**: {query}", expanded=True, state="complete"):
-                st.write(step)
-                # st.write(step[0].log)
-                # if step[1].get("completion"):
-                #     st.write(step[1]["completion"])
-                # else:
-                #     st.write(step[1])
-        print(msg.content)
-        st.write(msg.content)
+# avatars = {"human": "user", "ai": "assistant"}
+# for idx, msg in enumerate(msgs.messages):
+#     with st.chat_message(avatars[msg.type]):
+#         # Render intermediate steps if any were saved
+#         for step in st.session_state.steps.get(str(idx), []):
+#             log.info("step", step=step)
+#             if step[0].tool == "_Exception":
+#                 continue
+#             query = step[0].tool_input["query"]
+#             with st.status(label=f"**Critisim**: {query}", state="running"):
+#                  st.write("running")
+#             with st.status(label=f"**Critisim**: {query}", expanded=True, state="complete"):
+#                 st.write(step)
+#                 # st.write(step[0].log)
+#                 # if step[1].get("completion"):
+#                 #     st.write(step[1]["completion"])
+#                 # else:
+#                 #     st.write(step[1])
+#         print(msg.content)
+#         st.write(msg.content)
 
 if pdf_file is not None:
     # st.chat_message("user").write(prompt)
@@ -76,24 +76,15 @@ if pdf_file is not None:
     with open("markdown/sample.md") as f:
         markdown_text = f.read()
 
-
     # agent = load_plan_and_execute(context=markdown_text, model_name="gpt-3.5-turbo-16k", verbose=False)
 
     llm = ChatOpenAI(model_name="gpt-4")
     planner = load_critisim_planner(llm=llm)
     drafter = load_redrafting_chain(llm=llm)
-
     plan = planner.plan({ "input": markdown_text })
 
-
-    with st.chat_message("assistant"):
-        st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=True)
-        response = agent({ "input": markdown_text }, callbacks=[st_cb])
-
-        output = response["output"]
-        if isinstance(output, str):
-            st.write(output)
-        else:
-            st.write(output["action_input"])
-        if response.get("intermediate_steps"):
-            st.session_state.steps[str(len(msgs.messages) - 1)] = response["intermediate_steps"]
+    for plan in plan.steps:
+        st.status(label=f"**Critisim**: {plan.value}", state="running")
+        output = drafter({ "critisim": plan.value, "context": markdown_text })
+        st.status(label=f"**Critisim**: {plan.value}", state="running")
+        st.write(output["text"])
