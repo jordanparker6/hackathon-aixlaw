8bca916fe801b80080c8cf5fcba5d5b0f8ea3700
vchhajed <vchhajed@buffalo.edu>
2023-09-23 17:08:08 -0700

    updated code

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ import fitz  # PyMuPDF
 import tempfile
 import os
 import base64
+from services.langchain.chains import load_critisim_chain
+from dotenv import load_dotenv    
+load_dotenv()
 
 def pdf_to_markdown(pdf_file):
     doc = fitz.open(pdf_file)
@@ -15,15 +18,15 @@ def pdf_to_markdown(pdf_file):
 
     return markdown_text
 
-st.title("PDF to Markdown Converter")
+st.markdown("## 404 Not[:red] Found")
 
 # Upload a PDF file
-pdf_file = st.file_uploader("Upload a PDF file", type=["pdf"])
+pdf_file = st.file_uploader("404 Not Found", type=["pdf"])
 
 if pdf_file is not None:
     # Display the uploaded PDF
-    st.subheader("Uploaded PDF:")
-    st.write(pdf_file)
+    # st.subheader("Uploaded PDF:")
+    # st.write(pdf_file)
 
     # Create a temporary file for the uploaded PDF
     pdf_temp_file = tempfile.NamedTemporaryFile(delete=False, suffix=".pdf")
@@ -37,9 +40,18 @@ if pdf_file is not None:
     st.subheader("Markdown Output:")
     st.text(markdown_text)
 
+
+    chain = load_critisim_chain()
+    output = chain.run(markdown_text)
+
+    critisims = output.split("\n\n")
+
+    for critism in critisims:
+        st.write(critism)
+
     # Provide a download link for the uploaded PDF
-    st.markdown("### Download Uploaded PDF")
-    st.markdown(f"Download the uploaded PDF [here](data:application/pdf;base64,{base64.b64encode(open(pdf_temp_file.name, 'rb').read()).decode()})")
+    # st.markdown("### Download Uploaded PDF")
+    # st.markdown(f"Download the uploaded PDF [here](data:application/pdf;base64,{base64.b64encode(open(pdf_temp_file.name, 'rb').read()).decode()})")
 
     # Create a temporary file for the generated Markdown
     markdown_temp_file = tempfile.NamedTemporaryFile(delete=False, suffix=".md")
@@ -47,8 +59,8 @@ if pdf_file is not None:
     markdown_temp_file.close()
 
     # Provide a download link for the generated Markdown
-    st.markdown("### Download Markdown")
-    st.markdown(f"Download the generated Markdown [here](data:file/md;base64,{base64.b64encode(open(markdown_temp_file.name, 'rb').read()).decode()})")
+    # st.markdown("### Download Markdown")
+    # st.markdown(f"Download the generated Markdown [here](data:file/md;base64,{base64.b64encode(open(markdown_temp_file.name, 'rb').read()).decode()})")
 
     # Remove the temporary files
     os.unlink(pdf_temp_file.name)
